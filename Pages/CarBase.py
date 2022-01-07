1ff78d472a86f9b8063f0ffc496f16e74e6bbb93
Admin <admin@LiriQA1.local>
2022-01-07 13:32:30 +0530

    Done changes in CarBase class

--- Pages/CarBase.py
+++ Pages/CarBase.py
@@ -15,11 +15,13 @@ class CarBase:
         return self.driver.find_element_by_xpath(ConfigReader.readConfig("locators", "car_Title_XPATH")).text
 
     def getCarNameAndPrice(self):
+        # Adding this comments To check Jekins auto build triggers
         # Use find_elements
         carName = self.driver.find_elements_by_xpath(ConfigReader.readConfig("locators", "carName_XPATH"))
         carPrice = self.driver.find_elements_by_xpath(ConfigReader.readConfig("locators", "carPrice_XPATH"))
 
         for i in range (1, len(carPrice)):
+
             # print(carName[i].text + " Car price is: " + carPrice[i].text)
             print((carName[i].text+" Car price is: "+carPrice[i].text).encode('utf8')) # This is to avoid error in Jenkins
 
